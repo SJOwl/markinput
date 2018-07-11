40234e623636a5d6103d16aafcfa7ea7b9b83178
Evgeniy <vorobeievgeniy@gmail.com>
2018-07-11 18:45:26 +0300

    works on emulator

--- app/src/androidTest/java/com/example/sj/formattableedittext/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/sj/formattableedittext/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.sj.formattableedittext
 
-import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.TypeTextAction
+import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-
+import android.view.KeyEvent
+import org.hamcrest.CoreMatchers.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,180 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.example.sj.formattableedittext", appContext.packageName)
+
+    @get:Rule
+    var mActivityTestRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    // todo vorobei type into and check if strings are the same as expected
+
+    @Test fun addText(){
+        type("Hi, @Dm")
+        clickFirstItemAtList()
+        type("! How are you? @Alexa")
+        clickFirstItemAtList()
+        type("mentioned you!")
+//        checkText("Hi,@Dmitryw! How are you? @Alexandr Telegin mentioned you!")
+        sleep(5)
+    }
+
+    @Test fun removeText(){
+        type("Hi, @Dm")
+        clickFirstItemAtList()
+        type("! How are you?")
+        onView(withId(R.id.editText)).perform(replaceText(""))
+        sleep(5)
+    }
+
+    @Test fun removeLastFromName(){
+        type("Hi @Dm")
+        clickFirstItemAtList()
+        type(" ")
+        removeLastSymbols(3)
+        sleep(3)
+    }
+
+    /**
+     * Hi, @Dm
+     * clear text
+     */
+    @Test fun openList() {
+        type("Hi, ")
+        listHidden()
+        type("@")
+        listDisplayed()
+        type("D")
+        listDisplayed()
+        type("m")
+        listDisplayed()
+
+        listHidden()
+
+        type("@")
+        listDisplayed()
+        type("D")
+        listDisplayed()
+        type("m")
+        listDisplayed()
+    }
+
+    /**
+     * '@' at first of string
+     * select friend
+     */
+    @Test fun firstAt() {
+        clearInput()
+        type("@")
+        listDisplayed()
+        type("alexa")
+        listDisplayed()
+        clickFirstItemAtList()
+        listHidden()
+        checkText("@Alexandr Telegin ")
+
+        removeLastSymbols(1)
+        listHidden()
+
+        removeLastSymbols(1)
+        listHidden()
+
+        clickFirstItemAtList()
+        listHidden()
+        checkText("@Alexandr Telegin ")
+    }
+
+    @Test fun sendMail() {
+        val first = "Hi, this is my mail: mymail@"
+        val second = "gmail.com"
+        type(first)
+        listHidden()
+        type(second)
+        checkText("$first$second")
+    }
+
+    @Test fun removeMention() {
+        type("Hi, @x")
+        listDisplayed()
+        removeLastSymbols(2)
+        checkText("Hi, ")
+        listHidden()
+    }
+
+    /**
+     * Remove space between 2 mentions
+     */
+    @Test fun mentionTwoAndEditMiddle() {
+        type("Hi, @Alexa")
+        listDisplayed()
+        clickFirstItemAtList()
+        checkText("Hi, @Alexandr Telegin ")
+        listHidden()
+
+        type(", @Too")
+        listDisplayed()
+        clickFirstItemAtList()
+        checkText("Hi, @Alexandr Telegin, @Tookey Williams ")
+        listHidden()
+    }
+
+    @Test fun editBetweenTwoMentions() {
+        type("Hi, @Alexa")
+        clickFirstItemAtList()
+        checkText("Hi, @Alexandr Telegin ")
+        type(", @Too")
+        clickFirstItemAtList()
+        checkText("Hi, @Alexandr Telegin, @Tookey Williams ")
+
+        moveLeft(17)
+        type(" and ")
+        listHidden()
+        moveLeft(10)
+        removeLastSymbols()
+    }
+
+
+    fun listDisplayed() = onView(withId(R.id.tagFriendsRecycler)).check(matches(isDisplayed()))
+
+    fun listHidden() = onView(withId(R.id.tagFriendsRecycler)).check(matches(not(isDisplayed())))
+
+    fun removeLastSymbols(n: Int = 1) {
+        (1..n).forEach { onView(isRoot()).perform(pressKey(KeyEvent.KEYCODE_DEL)) }
+    }
+
+    fun moveLeft(n: Int) {
+        (1..n).forEach { onView(isRoot()).perform(pressKey(KeyEvent.KEYCODE_SYSTEM_NAVIGATION_LEFT)) }
+    }
+
+    fun clickFirstItemAtList() {
+        onView(withId(R.id.tagFriendsRecycler))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<FriendsAdapter.ItemViewHolder>(0, click()))
+    }
+
+    fun type(text: String) {
+        onView(withId(R.id.editText)).perform(closeSoftKeyboard())
+        onView(withId(R.id.editText)).perform(click())
+        onView(withId(R.id.editText)).perform(TypeTextAction(text)).perform(closeSoftKeyboard())
+    }
+
+    fun ty(text: String) {
+        onView(withId(R.id.editText)).perform(TypeTextAction(text))
+    }
+
+    fun checkText(text: String) {
+        onView(withId(R.id.editText)).check(matches(withText(text)))
+    }
+
+    fun clearInput() {
+        onView(withId(R.id.editText))
+                .perform(closeSoftKeyboard())
+                .perform(replaceText(""))
+    }
+
+    fun sleep(seconds: Int) {
+        try {
+            Thread.sleep(seconds * 1000L)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+
     }
 }
--- app/src/main/java/com/example/sj/formattableedittext/MainActivity.kt
+++ app/src/main/java/com/example/sj/formattableedittext/MainActivity.kt
@@ -18,18 +18,20 @@ class MainActivity : AppCompatActivity() {
 
     private val layoutId = R.layout.activity_main
 
-    private val userTags: MutableList<UserTag> = mutableListOf()
-
     private val friendsAdapter: FriendsAdapter by lazy {
         FriendsAdapter().apply {
             onItemClickListener = { user -> setUserId(user) }
         }
     }
 
-    private val inputs: List<InputItem> = mutableListOf()
+    @Volatile
+    var notToWatchText = false
+
+    private val inputs: MutableList<InputItem> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(layoutId)
 
         setRecycler()
@@ -37,65 +39,240 @@ class MainActivity : AppCompatActivity() {
         tagFriendButton.setOnClickListener { }
 
         closeTagFriensListButton.setOnClickListener {
-            Log.d("vorobeisj", "close dialog clicked")
+            //            Log.d("vorobeisj", "close dialog clicked")
             tagFriendsRecycler.setInvisible()
         }
 
+        inputs.add(InputItemText())
+
         editText.addTextChangedListener(object : EditableTextWatcher() {
+            private var added: Int = 0
+            private var removed: Int = 0
+            private var replaced: Int = 0
+            private var start: Int = 0
+            private var after: Int = 0
+
+            override fun beforeTextChange(s: CharSequence, start: Int, count: Int, after: Int) {
+                if (notToWatchText) return
+                this.start = start
+                this.after = after
+                added = if (count == 0) after else 0
+                removed = if (after == 0) count else 0
+                replaced = if (count != 0 && after != 0) after else 0 // from $start $count symbols replaced by $after
+
+//                this.start = start + count
+//                this.after = start + after
+//                added = if (after - count > 0) after - count else 0
+//                removed = if (after - count < 0) count-after else 0
+//                replaced = if (count != 0 && after != 0) after else 0 // from $start $count symbols replaced by $after
+//                beforeReplace = if (replaced != 0) count else 0
+
+                Log.d("vorobeisj", "before added = $added, removed =$removed, replaced = $replaced")
+            }
+
             override fun onTextChange(s: CharSequence,
                                       wordStart: Int,
-                                      oldTextLengthFromStart: Int,
-                                      newTextLengthFromStart: Int) {
-                // check for " @" or "^@" - create new UserItem and add to inputs
+                                      before: Int,
+                                      count: Int) {
+                if (notToWatchText) return
 
-                // change text
+                if (added != 0) editAdd(s, start, added)
+                if (removed != 0) editRemoved(s, start, removed) // if user removed - concat with text
+                if (replaced != 0) editReplace(s, start, before, replaced)
 
-                // request friend if string not empty
+                concatTextItems()
 
+                printInputs()
+
+//                recalcStringFromInputs(inputs)
+
+                showRich(start + after)
+
+                Log.d("vorobeisj", "***************************************")
             }
         })
 
         tagFriendsRecycler.setInvisible()
     }
 
-    fun tagFriend(s: CharSequence,
-                  wordStart: Int,
-                  oldTextLengthFromStart: Int,
-                  newTextLengthFromStart: Int,
-                  inputs: List<InputItem>) {
-        Log.d("vorobeisj", "string  ")
-        val end = wordStart + newTextLengthFromStart
-        val newText = s.subSequence(wordStart, end)
-        if (newText == "@") {
-            if (((end > 0 && s[end - 1] == ' ') ||
-                            (end < s.length - 1 && s[end + 1] == ' '))) { // "<text> @| <text>"
-                // find place for new mention, split previously created text items
-            } else if (end == 1) { // "@| <text>"
-                // put new mention as first item
+    fun printInputs() {
+        Log.d("vorobeisj", "inputs *******************************")
+        inputs.forEach {
+            Log.d("vorobeisj", "${it.type} s=${it.start} e=${it.end} text=\'${it.text}\'")
+        }
+        Log.d("vorobeisj", "**************************************************************")
+    }
+
+    /**
+     * iterate through list and concat neighbor text items
+     */
+    fun concatTextItems() {
+        var i = 0
+        while (i < inputs.size) {
+            if (i + 1 < inputs.size && inputs[i + 1].type == InputTypes.TYPE_TEXT && inputs[i].type == InputTypes.TYPE_TEXT) {
+                inputs[i].append(inputs[i + 1].text)
+                inputs.removeAt(i + 1)
+                i--
+            }
+            i++
+        }
+    }
+
+    fun isConcatNeeded(inputs: List<InputItem>): Boolean {
+        for (i in 0 until inputs.size)
+            if (i + 1 < inputs.size &&
+                    inputs[i].type == InputTypes.TYPE_TEXT &&
+                    inputs[i + 1].type == InputTypes.TYPE_TEXT) return true
+        return false
+    }
+
+    fun editAdd(newString: CharSequence, start: Int, count: Int) {
+        recalcStringFromInputs(inputs)
+
+        val addedText = newString.substring(start, start + count)
+        Log.d("vorobeisj", "add \"$addedText\" start=$start, count=$count")
+
+        var edited = getItemToAdd(start)
+                ?: throw IllegalStateException("edited item can not be null")
+
+        if ((addedText == "@" && start == 0) ||
+                (addedText == "@" && start - edited.start - 1 > 0 &&
+                        edited.text[start - edited.start - 1] == ' ')) {
+
+            val left = InputItemText(edited.text.substring(0, start - edited.start))
+            val right = InputItemText(edited.text.substring(start - edited.start, edited.end - edited.start))
+            val index = inputs.indexOf(edited)
+            inputs.removeAt(index)
+            inputs.add(index, left)
+            val user = InputItemUser()
+            inputs.add(index + 1, user)
+            queryUser(user)
+            inputs.add(index + 2, right)
+        } else {
+            if (edited.end == start)
+                edited.append(addedText)
+            else {
+                if (edited is InputItemUser) edited = replaceWithTextItem(edited)
+                edited.add(start - edited.start, addedText)
+            }
+        }
+        if (edited is InputItemUser) queryUser(edited)
+
+        recalcStringFromInputs(inputs)
+        editText.placeCursor(start + count)
+    }
+
+    fun getItemToAdd(start: Int): InputItem? {
+        recalcStringFromInputs(inputs)
+        val edited = inputs.filter { it.isInRange(start) }
+        if (edited.isNotEmpty()) return edited[0]
+        else return null
+    }
+
+    fun getItemToRemove(start: Int, count: Int) {
+        recalcStringFromInputs(inputs)
+        val editStart = inputs.filter { it.isInRange(start) }
+        val end = start + count
+        val editEnd = inputs.filter { it.isInRange(end) }
+    }
+
+    fun getPrevious(start: Int): InputItem? {
+        val edited = inputs.filter { it.isInRange(start) }
+        if (edited.isNotEmpty()) {
+            val index = inputs.indexOf(edited[0])
+            return if (index > 0) inputs[index - 1] else null
+        }
+        return null
+    }
+
+    fun recalcStringFromInputs(inputs: List<InputItem>): String {
+//        Log.w("vorobeisj", "recalc inputs called") // todo vorobei check calls are needed
+        val stringBuilder = StringBuilder()
+        inputs.forEach { input ->
+            input.start = stringBuilder.length
+            stringBuilder.append(input.text)
+            input.end = stringBuilder.length
+        }
+        return stringBuilder.toString()
+    }
+
+    fun editRemoved(newString: CharSequence, start: Int, count: Int) {
+        Log.d("vorobeisj", "remove start=$start, count=$count")
+
+        recalcStringFromInputs(inputs)
+        val editStartList = inputs.filter { it.isInRemoveRange(start) }
+        var end = start + count - 1
+        end = Math.max(start, end)
+        val editEndList = inputs.filter { it.isInRemoveRange(end) }
+
+        if (editStartList.isEmpty()) throw IllegalStateException("start of removing is empty")
+        if (editEndList.isEmpty()) throw IllegalStateException("end of removing is empty")
+        val editStartIndex = inputs.indexOf(editStartList[0])
+        val editEndIndex = inputs.indexOf(editEndList[0])
+
+        if (editStartIndex < editEndIndex) {
+            // remove all items between
+            (editEndIndex - 1 downTo editStartIndex + 1)
+                    .forEach { inputs.removeAt(it) }
+            // edit edge items
+            inputs[editEndIndex].run { if (this is InputItemUser) replaceWithTextItem(this, editEndIndex) }
+            recalcStringFromInputs(inputs)
+            inputs[editEndIndex].run { (this as InputItemText).removeRange(this.start, start + count) }
+
+            inputs[editStartIndex].run { if (this is InputItemUser) replaceWithTextItem(this, editStartIndex) }
+            recalcStringFromInputs(inputs)
+            inputs[editStartIndex].run { this.removeRange(start, this.end) }
+            // check remove user-text and text-user (user-user at one user) (text-text at one text)
+            // check indexes
+        } else {
+            inputs[editStartIndex].run {
+                this.removeRange(start, start + count)
+                if (this is InputItemUser) replaceWithTextItem(this, editStartIndex)
             }
         }
     }
 
-    fun getSearchRequest(inputs: List<InputItem>): String? {
-        val edited = inputs.filter { it.type == InputTypes.TYPE_USER }.filter { (it as InputItemUser).userId.isNotEmpty() }
-        if (edited.size > 1) throw IllegalStateException("At one time 2 mentions are edited")
-        if (edited.isNotEmpty()) return edited[0].text
-        return null
+    /**
+     * @param index: index of @userItem
+     */
+    fun replaceWithTextItem(userItem: InputItemUser, index: Int = inputs.indexOf(userItem)): InputItemText {
+        val newText = InputItemText(userItem.text)
+        inputs.removeAt(index)
+        inputs.add(index, newText)
+        hideFriendsList()
+        return newText
+    }
+
+    fun editReplace(newString: CharSequence, start: Int, countWas: Int, countNow: Int) {
+        Log.d("vorobeisj", "editReplace start=$start, countWas=$countWas, countNow=$countNow")
+        editRemoved(getStringFromInputs(inputs), start, countWas)
+        editAdd(newString, start, countNow)
+    }
+
+    fun showRich(cursorPos: Int) {
+        notToWatchText = true
+        editText.setText(getSpannableFromInputs(inputs))
+        editText.placeCursor(cursorPos)
+        notToWatchText = false
+    }
+
+    fun queryUser(user: InputItemUser) {
+        suggestFriends()
+        getUsersByQuery(user.displayName).subscribe { items ->
+            friendsAdapter.items = items
+        }
+    }
+
+    /**
+     * create new UserItem and add to list
+     */
+    fun openUserItem(start: Int) {
+
     }
 
     fun getSpannableFromInputs(inputs: List<InputItem>): SpannableString {
-        val stringBuilder = StringBuilder()
-        val tags: List<Range> = mutableListOf()
-        val max = inputs.size - 1
-        inputs.forEachIndexed { index, inputItem ->
-            val start = stringBuilder.length
-            stringBuilder.append("${inputItem.text}")
-            val end = stringBuilder.length
-            if (index < max) stringBuilder.append(" ")
-        }
-
-        return SpannableString(stringBuilder.toString()).apply {
-            tags.forEach { tag ->
+        return SpannableString(recalcStringFromInputs(inputs)).apply {
+            inputs.filter { it is InputItemUser }.forEach { tag ->
                 try {
                     setSpan(StyleSpan(Typeface.BOLD), tag.start, tag.end, 0)
                     setSpan(ForegroundColorSpan(Color.parseColor("#ff9900")), tag.start, tag.end, 0)
@@ -106,24 +283,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun getStringFromInputs(inputs: List<InputItem>): String = inputs.joinToString(" ") { it.text }
-
-    fun changeText(s: CharSequence,
-                   wordStart: Int,
-                   oldTextLengthFromStart: Int,
-                   newTextLengthFromStart: Int,
-                   inputs: List<InputItem>): String {
-        // find replaced text at inputs
-        // text replace with text
-        // if user's display name changed - change it's type, put new item with text, not user
-        // if user's name changed, but user has no id - return user name to request
-
-
-        return ""
+    fun getStringFromInputs(inputs: List<InputItem>): String {
+        val res = inputs.joinToString("") { it.text }
+        return res
     }
 
     fun setUserId(user: UserItem) {
-        // find input item with text = requestedString and empty id, set id and name
+//        Log.d("vorobeisj", "setUserId==========================================")
+        val openedUsers = inputs.filter { it is InputItemUser }.filter { (it as InputItemUser).userId.isEmpty() }
+        if (openedUsers.size > 1) throw IllegalStateException("2 users can not be edited at the same time")
+        if (openedUsers.size == 1) {
+            (openedUsers[0] as InputItemUser).run {
+                userId = user.id
+                displayName = user.displayName
+            }
+            // add text item after user
+            val index = inputs.indexOf(openedUsers[0])
+            val newText = InputItemText(" ")
+            inputs.add(index + 1, newText)
+
+            hideFriendsList()
+            recalcStringFromInputs(inputs)
+            showRich(newText.end)
+        }
+        if (openedUsers.isEmpty()) throw IllegalStateException("attempt to set user id when no edited userInputs")
     }
 
     fun suggestFriends() = tagFriendsRecycler.setVisible()
@@ -207,13 +390,51 @@ enum class InputTypes {
 
 open class InputItem(var type: InputTypes, text: String = "") {
     open var text: String = text
+    var start = 0
+    var end = 0
+    fun isInRange(index: Int) = index in start..end
+
+    fun isInRemoveRange(index: Int) = index in start until end
+
+    open fun append(toAppend: String) {
+        this.text = "${this.text}$toAppend"
+    }
+
+    /**
+     * pos : 0 is at begin of @text
+     */
+    open fun add(pos: Int, toAdd: String) {
+        try {
+            this.text = this.text.replaceRange(pos, pos, toAdd)
+        } catch (e: IndexOutOfBoundsException) {
+            Log.d("vorobeisj", "IOOBE")
+        }
+    }
+
+    open fun removeRange(start: Int, end: Int) {
+        text = text.replaceRange(start - this.start, end - this.start, "")
+    }
+
+    override fun toString(): String = text
 }
 
-class InputItemText(text: String) : InputItem(InputTypes.TYPE_TEXT, text)
+class InputItemText(text: String = "") : InputItem(InputTypes.TYPE_TEXT, text)
 
-class InputItemUser(displayName: String, val prefix: String = "@", var userId: String = "") : InputItem(InputTypes.TYPE_USER, displayName) {
+class InputItemUser(var displayName: String = "", var prefix: String = "@", var userId: String = "") : InputItem(InputTypes.TYPE_USER, displayName) {
     override var text: String = displayName
-        get() = "$prefix$field"
-}
+        get() = "$prefix$displayName"
 
-data class Range(var start: Int = 0, var end: Int = 0)
+    override fun append(toAppend: String) {
+        this.displayName = "${this.displayName}$toAppend"
+    }
+
+    override fun removeRange(start: Int, end: Int) {
+        try {
+            val s = start - this.start - 1
+            val e = end - this.start - 1
+            displayName = displayName.replaceRange(s, e, "")
+        } catch (e: IndexOutOfBoundsException) {
+            Log.d("vorobeisj", "")
+        }
+    }
+}
